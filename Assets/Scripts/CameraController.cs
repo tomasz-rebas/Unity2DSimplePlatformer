fc03db2d1ea2732d7f5e433eb57e53cdb9673c27
tomasz-rebas <42824472+tomasz-rebas@users.noreply.github.com>
2021-02-01 20:46:27 +0100

    Initial commit

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [System.Serializable] 
+    public class Bounds
+    {
+        public float left;
+        public float right;
+        public float down;
+    }
+
+    public Transform playerTransform;
+    public Transform cameraTransform;
+    public Bounds bounds = new Bounds() ; 
+
+    void Update ()
+    {
+        float _playerX = playerTransform.position.x;
+        float _playerY = playerTransform.position.y;
+
+        float _newCameraPosX = playerTransform.position.x;
+        float _newCameraPosY = playerTransform.position.y;
+
+        if (_playerX <= bounds.left)
+        {
+            _newCameraPosX = bounds.left;
+        }
+
+        if (_playerX >= bounds.right)
+        {
+            _newCameraPosX = bounds.right;
+        }
+
+        if (_playerY <= bounds.down)
+        {
+            _newCameraPosY = bounds.down;
+        }
+
+        cameraTransform.position = new Vector3
+        (
+            _newCameraPosX,
+            _newCameraPosY,
+            cameraTransform.position.z
+        );
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody2D rb2d;
+    public float sideMovementForce;
+    public float jumpForce;
+
+    private Vector2 jumpVector;
+    public bool isPlayerGrounded;
+    public bool canDoubleJump;
+
+    void Start ()
+    {
+        jumpVector = new Vector2 (0, jumpForce);
+    }
+
+    void Update ()
+    {
+        if (Input.GetKey("a"))
+        {
+            rb2d.velocity = new Vector2 (-sideMovementForce, rb2d.velocity.y);
+        }
+
+        if (Input.GetKey("d"))
+        {
+            rb2d.velocity = new Vector2 (sideMovementForce, rb2d.velocity.y);
+        }
+
+        if (Input.GetKey("space"))
+        {
+            if (isPlayerGrounded)
+            {
+                canDoubleJump = true;
+                Jump();
+            }
+            else if (canDoubleJump)
+            {
+                canDoubleJump = false;
+                Jump();
+            }
+        }
+    }
+
+    void Jump ()
+    {
+        rb2d.velocity = new Vector2 (rb2d.velocity.x, 0);
+        rb2d.AddForce(jumpVector);
+    }
+
+    void OnCollisionEnter2D (Collision2D c)
+    {
+        if (c.gameObject.name == "Floor")
+        {
+            isPlayerGrounded = true;
+        }
+    }
+
+    void OnCollisionExit2D (Collision2D c)
+    {
+        if (c.gameObject.name == "Floor")
+        {
+            isPlayerGrounded = false;
+        }
+    }
+}
