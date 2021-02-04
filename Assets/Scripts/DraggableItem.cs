720ed0c9f014a19b4c842f6ee42c03da16e77da1
tomasz-rebas <42824472+tomasz-rebas@users.noreply.github.com>
2021-02-04 18:03:47 +0100

    Using ScreenToWorldPoint to drag the item

--- Assets/Scripts/DraggableItem.cs
+++ Assets/Scripts/DraggableItem.cs
@@ -5,7 +5,22 @@ using UnityEngine.EventSystems;
 
 public class DraggableItem : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler
 {
-    public void OnDrag (PointerEventData eventData) {}
+    public Transform item;
+
+    private Camera cam;
+
+    void Start()
+    {
+        cam = Camera.main;
+    }
+
+    public void OnDrag (PointerEventData eventData)
+    {
+        float _x = eventData.position.x;
+        float _y = eventData.position.y;
+
+        item.position = cam.ScreenToWorldPoint(new Vector3 (_x, _y, cam.nearClipPlane));
+    }
 
     public void OnBeginDrag (PointerEventData eventData) {}
 
@@ -14,6 +29,7 @@ public class DraggableItem : MonoBehaviour, IDragHandler, IBeginDragHandler, IEn
         if (eventData.hovered.Count > 0)
         {
             string _objName = eventData.hovered[eventData.hovered.Count - 1].name;
+
             if (_objName == "ItemSlot1" || _objName == "ItemSlot2")
             {
                 Debug.Log("Key dropped in the item slot.");
--- Assets/Scripts/ItemSlot.cs
+++ Assets/Scripts/ItemSlot.cs
@@ -14,6 +14,7 @@ public class ItemSlot : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDrag
         if (eventData.hovered.Count > 0)
         {
             string _objName = eventData.hovered[eventData.hovered.Count - 1].name;
+            
             if (_objName == "DoorBlue" || _objName == "DoorGreen")
             {
                 Debug.Log("Key used to open the door.");
