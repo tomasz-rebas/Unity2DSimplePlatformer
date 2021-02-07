b289fd6c68207240a633a8c169c1cf6d84d6ec08
tomasz-rebas <42824472+tomasz-rebas@users.noreply.github.com>
2021-02-07 20:32:51 +0100

    Script component for an item slot (getter and setter for isTaken bool)

--- Assets/Scripts/ItemSlot.cs
+++ Assets/Scripts/ItemSlot.cs
@@ -5,7 +5,16 @@ using UnityEngine.EventSystems;
 
 public class ItemSlot : MonoBehaviour/*, IDragHandler, IBeginDragHandler, IEndDragHandler*/
 {
-    public bool isTaken = false;
+    private bool _isTaken = false;
+
+    public bool isTaken
+    {
+        get => _isTaken;
+        set
+        {
+            _isTaken = value;
+        }
+    }
 
     // public void OnDrag (PointerEventData eventData) {}
     // public void OnBeginDrag (PointerEventData eventData) {}
