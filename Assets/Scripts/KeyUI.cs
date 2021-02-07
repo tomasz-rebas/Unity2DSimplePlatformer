c44dd095e473b6cdb45187945c96e3971c367997
tomasz-rebas <42824472+tomasz-rebas@users.noreply.github.com>
2021-02-07 23:31:35 +0100

    Drag a key from the UI and detect if released on the door

--- Assets/Scripts/KeyUI.cs
+++ Assets/Scripts/KeyUI.cs
@@ -6,7 +6,10 @@ using UnityEngine.UI;
 
 public class KeyUI : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler
 {
+    public Transform door;
+
     private RectTransform keyUi;
+    private Vector3 keyUiDefaultPosition;
 
     void Start ()
     {
@@ -15,16 +18,32 @@ public class KeyUI : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHan
 
     public void OnDrag (PointerEventData eventData)
     {
-
+        keyUi.position = eventData.position;
     }
 
     public void OnBeginDrag (PointerEventData eventData)
     {
-        
+        keyUiDefaultPosition = keyUi.position;
     }
 
     public void OnEndDrag (PointerEventData eventData)
     {
-
+        Vector2 worldPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        RaycastHit2D hit = Physics2D.Raycast(worldPoint, Vector2.zero);
+        if (hit.collider != null)
+        {
+            if (hit.collider.name == "DoorBlue")
+            {
+                Debug.Log("Blue door hit.");
+            }
+            else
+            {
+                keyUi.position = keyUiDefaultPosition;
+            }
+        }
+        else
+        {
+            keyUi.position = keyUiDefaultPosition;
+        }
     }
 }
