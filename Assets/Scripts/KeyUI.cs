52428900c622b26a4063f4d9f14eb3e6015d9c4f
tomasz-rebas <42824472+tomasz-rebas@users.noreply.github.com>
2021-02-07 21:43:50 +0100

    Create script component for UI key

---
+++ Assets/Scripts/KeyUI.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class KeyUI : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler
+{
+    private RectTransform keyUi;
+
+    void Start ()
+    {
+        keyUi = gameObject.GetComponent<RectTransform>();
+    }
+
+    public void OnDrag (PointerEventData eventData)
+    {
+
+    }
+
+    public void OnBeginDrag (PointerEventData eventData)
+    {
+        
+    }
+
+    public void OnEndDrag (PointerEventData eventData)
+    {
+
+    }
+}
