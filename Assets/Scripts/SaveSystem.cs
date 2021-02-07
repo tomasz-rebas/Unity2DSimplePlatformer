466f734acb89cf32086910f3ea6ba18b69a11b72
tomasz-rebas <42824472+tomasz-rebas@users.noreply.github.com>
2021-02-08 00:38:21 +0100

    Create save/load file system

---
+++ Assets/Scripts/SaveSystem.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+public static class SaveSystem
+{
+    private static string path = Application.persistentDataPath + "/save.dat";
+
+    public static void SaveGame ()
+    {
+        BinaryFormatter formatter = new BinaryFormatter();
+        FileStream stream = new FileStream(path, FileMode.Create);
+        formatter.Serialize(stream, data);
+        stream.Close();
+    }
+
+    public static SaveData LoadGame ()
+    {
+        if (File.Exists(path))
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            FileStream stream = new FileStream(path, FileMode.Open);
+            SaveData data = formatter.Deserialize(stream) as SaveData;
+            stream.Close();
+            return data;
+        }
+        else
+        {
+            Debug.LogError("Save file not found.");
+            return null;
+        }
+
+    }
+}
