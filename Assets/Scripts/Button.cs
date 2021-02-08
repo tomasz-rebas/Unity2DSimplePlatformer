4e0e2b594e5ab068bd013fb4b5f255948b3dbe54
tomasz-rebas <42824472+tomasz-rebas@users.noreply.github.com>
2021-02-08 15:39:08 +0100

    Deselect button after clicking it (to prevent further resetting when jumping)

---
+++ Assets/Scripts/Button.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Button : MonoBehaviour, IPointerUpHandler
+{
+    public void OnPointerUp (PointerEventData eventData)
+    {
+        EventSystem.current.SetSelectedGameObject(null);
+    }
+}
