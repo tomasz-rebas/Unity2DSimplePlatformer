877c91696348617964dfd08d5a1bda37f0bd47f4
tomasz-rebas <42824472+tomasz-rebas@users.noreply.github.com>
2021-02-08 01:14:52 +0100

    Add SaveData script

---
+++ Assets/Scripts/SaveData.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class SaveData
+{
+    public float[] position;
+    public string[] inventory;
+
+    public SaveData (PlayerMovement playerMovement)
+    {
+        position = new float[3];
+        position[0] = playerMovement.transform.position.x;
+        position[1] = playerMovement.transform.position.y;
+        position[2] = playerMovement.transform.position.z;
+    }
+}
