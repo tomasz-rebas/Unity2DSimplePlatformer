a720c2fd13e8c18313b6b5aa36c7acbfe122932a
tomasz-rebas <42824472+tomasz-rebas@users.noreply.github.com>
2021-02-08 14:36:25 +0100

    Load player position on start

--- Assets/Scripts/SaveRef.cs
+++ Assets/Scripts/SaveRef.cs
@@ -9,6 +9,18 @@ public class SaveRef : MonoBehaviour
     public Transform keyGreen;
     public RectTransform itemSlots;
 
+    void Start ()
+    {
+        SaveData data = SaveSystem.LoadGame();
+
+        player.position = new Vector3
+        (
+            data.position[0],
+            data.position[1],
+            data.position[2]
+        );
+    }
+
     void OnApplicationQuit()
     {
         SaveSystem.SaveGame(this);
