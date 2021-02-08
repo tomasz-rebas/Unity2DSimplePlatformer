40960511cb6b74003c892366ea9e607658105125
tomasz-rebas <42824472+tomasz-rebas@users.noreply.github.com>
2021-02-08 15:57:09 +0100

    Code cleaning

--- Assets/Scripts/SaveSystem.cs
+++ Assets/Scripts/SaveSystem.cs
@@ -13,8 +13,6 @@ public static class SaveSystem
         FileStream stream = new FileStream(path, FileMode.Create);
         SaveData data = new SaveData(saveRef);
         formatter.Serialize(stream, data);
-        // Debug.Log(data.doorBlueOpen);
-        // Debug.Log(data.doorBlueOpen);
         stream.Close();
     }
 
@@ -26,11 +24,6 @@ public static class SaveSystem
             FileStream stream = new FileStream(path, FileMode.Open);
             SaveData data = formatter.Deserialize(stream) as SaveData;
             stream.Close();
-            // Debug.Log(data.position[0]);
-            // Debug.Log(data.position[1]);
-            // Debug.Log(data.position[2]);
-            // Debug.Log(data.inventory[0]);
-            // Debug.Log(data.inventory[1]);
             return data;
         }
         else
